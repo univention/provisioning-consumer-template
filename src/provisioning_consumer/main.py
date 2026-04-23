a4c517d2eb95f0af71e61703e1eba730e134c1e8
Sönke Schwardt-Krummrich <schwardt@univention.de>
2026-04-23 13:45:36 +0200

    fix: use consumer lib in main.py

--- src/provisioning_consumer/main.py
+++ src/provisioning_consumer/main.py
@@ -1,13 +1,26 @@
-import time
+# SPDX-FileCopyrightText: 2026 Univention GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
 
-from provisioning_consumer_lib import ConsumerModule, EventHandler, DN
+from loguru import logger
+from provisioning_consumer_lib import ConsumerModule, UDMEventHandler, AttributeMapping
+
+
+class MyEventHandler(UDMEventHandler):
+    def _handle_create(self, metadata: AttributeMapping, new: AttributeMapping) -> None:
+        self.logger.info('_handle_create has been called for %s', new["dn"])
+
+    def _handle_modify(self, metadata: AttributeMapping, old: AttributeMapping, new: AttributeMapping, has_moved: bool) -> None:
+        self.logger.info('_handle_modify has been called for %s', new["dn"])
+
+    def _handle_remove(self, metadata: AttributeMapping, old: AttributeMapping) -> None:
+        self.logger.info('_handle_remove has been called for %s', old["dn"])
 
 
 def main() -> None:
-    print("Hello, World!")
-
-    while True:
-        time.sleep(1)
+    provisioning_url = "https://fqdn-of-primary-directory-node/univention/provisioning/"
+    event_handler = MyEventHandler(logger)
+    consumer = ConsumerModule(handler=event_handler, name="TestConsumer", provisioning_url=provisioning_url)
+    consumer.loop()
 
 
 if __name__ == "__main__":
